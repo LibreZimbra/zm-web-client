6992bbd4a821d670f3c17a2163e952b6b0665bae
Enrico Weigelt, metux IT consult <info@metux.net>
2022-01-25 12:21:23 +0100

    pkg-builder.pl: drop useless dummy package zimbra-store-base

--- pkg-builder.pl
+++ pkg-builder.pl
@@ -76,7 +76,7 @@ my %PKG_GRAPH = (
       revision   => $revision,
       hard_deps  => [],
       soft_deps  => [],
-      other_deps => ["zimbra-store-base"],
+      other_deps => [],
       replaces   => ["zimbra-store"],
       dir_list   => ['/opt/zimbra/conf/templates/*',
                      '/opt/zimbra/jetty_base/webapps/zimbra/META-INF/*',
